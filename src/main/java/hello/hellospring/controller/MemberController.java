f5c273afcd706d30a92937f79abd889b5bb26042
Jiwon <mon99745@gmail.com>
2021-11-04 19:22:28 +0900

    member -update

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -13,10 +13,19 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
-
-    @GetMapping (value = "/members/new")
+    //회원 컨트롤러에서 조회 기능
+    @GetMapping(value = "/members/new")
     public String createForm(){
         return "member/createMemberForm";
     }
+//// 회원 컨트롤러에서 회원을 실제 등록하는 기능
+//@PostMapping(value = "/members/new")
+//public String create(MemberForm form) {
+//    Member member = new Member();
+//    member.setName(form.getName());
+//    memberService.join(member);
+//    return "redirect:/";
+//}
+
 
 }
